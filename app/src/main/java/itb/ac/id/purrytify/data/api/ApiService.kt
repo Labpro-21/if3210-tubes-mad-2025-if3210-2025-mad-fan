fed6c02ea0b9334b361b2468053becaf0e660dff
FedrianzD <fedrianzd@gmail.com>
2025-03-31 20:42:53 +0700

    feat: add api call for profile and profile-picture

--- app/src/main/java/itb/ac/id/purrytify/data/api/ApiService.kt
+++ app/src/main/java/itb/ac/id/purrytify/data/api/ApiService.kt
@@ -2,11 +2,21 @@ package itb.ac.id.purrytify.data.api
 
 import itb.ac.id.purrytify.data.model.LoginRequest
 import itb.ac.id.purrytify.data.model.LoginResponse
+import itb.ac.id.purrytify.data.model.ProfileResponse
+import okhttp3.ResponseBody
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiService {
-    @POST("login")
+    @POST("api/login")
     suspend fun login(@Body request: LoginRequest): LoginResponse
+
+    @GET("api/profile")
+    suspend fun getProfile(): ProfileResponse
+
+    @GET("uploads/profile-picture/{profilePhotoPath}")
+    suspend fun getProfilePicture(@Path("profilePhotoPath") profilePhotoPath: String): Response<ResponseBody>
 }
---
+++ app/src/main/java/itb/ac/id/purrytify/data/model/ProfileResponse.kt
@@ -0,0 +1,11 @@
+package itb.ac.id.purrytify.data.model
+
+data class ProfileResponse(
+    val id: Int,
+    val username: String,
+    val email: String,
+    val profilePhoto: String,
+    val location: String,
+    val createdAt: String,
+    val updatedAt: String,
+)

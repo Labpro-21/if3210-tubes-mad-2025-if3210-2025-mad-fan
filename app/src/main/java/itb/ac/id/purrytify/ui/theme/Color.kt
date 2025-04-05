c5cb955b81402f5a8c9bbcf0e6c9254f3e25496b
FedrianzD <fedrianzd@gmail.com>
2025-04-05 13:05:45 +0700

    feat: add color theme

--- app/src/main/java/itb/ac/id/purrytify/ui/theme/Color.kt
+++ app/src/main/java/itb/ac/id/purrytify/ui/theme/Color.kt
@@ -3,6 +3,13 @@ package itb.ac.id.purrytify.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val BlackBackground = Color(0xff121212)
+val WhiteFont = Color(0xffFFFFFF)
+val SpotifyGreen = Color(0xff1db954)
+val Grey13 = Color(0xff212121)
+val DavyGrey = Color(0xff535353)
+val Grey70 = Color(0xffB3B3B3)
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
--- app/src/main/java/itb/ac/id/purrytify/ui/theme/Theme.kt
+++ app/src/main/java/itb/ac/id/purrytify/ui/theme/Theme.kt
@@ -12,32 +12,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = SpotifyGreen,
+    onPrimary = WhiteFont,
+    background = BlackBackground,
+    onBackground = WhiteFont,
+    surface = Grey13,
+    onSurface = WhiteFont,
+    secondary = DavyGrey,
+    onSecondary = WhiteFont,
+    tertiary = Grey70
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = SpotifyGreen,
+    onPrimary = WhiteFont,
+    background = BlackBackground,
+    onBackground = WhiteFont,
+    surface = Grey13,
+    onSurface = WhiteFont,
+    secondary = DavyGrey,
+    onSecondary = WhiteFont,
+    tertiary = Grey70
 )
 
 @Composable
 fun PurrytifyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {

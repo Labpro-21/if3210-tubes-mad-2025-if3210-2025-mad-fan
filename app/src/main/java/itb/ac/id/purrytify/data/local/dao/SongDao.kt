e2918beb7fb7fe205424da5a704e2996fc5720d7
AureliusJustin <88382259+AureliusJustin@users.noreply.github.com>
2025-04-12 20:14:39 +0700

    fix: set new songs to include played songs

--- app/src/main/java/itb/ac/id/purrytify/data/local/dao/SongDao.kt
+++ app/src/main/java/itb/ac/id/purrytify/data/local/dao/SongDao.kt
@@ -14,10 +14,10 @@ interface SongDao {
     @Query("SELECT * FROM song WHERE userID = :userId AND isLiked = true")
     fun getLiked(userId: Int): Flow<List<Song>>
 
-    @Query("SELECT * FROM song WHERE userID = :userId AND lastPlayed IS NULL ORDER BY createdAt DESC")
+    @Query("SELECT * FROM song WHERE userID = :userId ORDER BY createdAt DESC LIMIT 10")
     fun getNew(userId: Int): Flow<List<Song>>
 
-    @Query("SELECT * FROM song WHERE userID = :userId AND lastPlayed IS NOT NULL ORDER BY lastPlayed DESC")
+    @Query("SELECT * FROM song WHERE userID = :userId AND lastPlayed IS NOT NULL ORDER BY lastPlayed DESC LIMIT 10")
     fun getRecentlyPlayed(userId: Int): Flow<List<Song>>
 
     @Query("SELECT COUNT(*) FROM song WHERE userID = :userId")
